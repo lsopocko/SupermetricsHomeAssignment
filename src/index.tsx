78433eba417329dd707a93e53cc936572eb75d7d
Łukasz Sopoćko <lukasz.sopocko@gmail.com>
2021-11-17 14:40:03 +0100

    Fix default route

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import "normalize.css";
@@ -19,6 +19,7 @@ ReactDOM.render(
         <Routes>
           <Route path="login" element={<Login />} />
           <Route path="/" element={<App />}>
+            <Route path="" element={<Navigate to="posts"/>} />
             <Route path="posts" element={
               <AuthRoute>
                 <Posts />
